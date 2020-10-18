6712a788a48893dbc44144e1a0cd0faf3ab0283a
TK <dk@terminalkitten.com>
2020-10-18 22:01:49 +0200

    cleanup, add Makefile for worker

--- demo/pdf/views.py
+++ demo/pdf/views.py
@@ -1,5 +1,3 @@
-from random import randint
-
 import pdfgen
 from io import BytesIO
 import asyncio
@@ -11,12 +9,7 @@ from django.shortcuts import render
 from django.views.generic import View
 from pdf_reactor import PdfReactor
 
-import nanoid
-from nanoid.resources import alphabet
-
 reactor = PdfReactor()
-
-from wsgiref.util import FileWrapper
 
 
 class PdfAsyncView(View):
@@ -27,22 +20,15 @@ class PdfAsyncView(View):
         return view
 
     async def post(self, request):
-
-        job_id = nanoid.generate(alphabet=alphabet[2:])
-        output = f'files/{job_id}.pdf'
-
         path = await pdfgen.from_url(
             request.build_absolute_uri(), None, options={'printBackground': True}
         )
-
         file_stream = BytesIO(path)
 
-        # generate the file
+        # generate downloadable file
         response = HttpResponse(file_stream.getvalue(), content_type='application/pdf')
-        response['Content-Disposition'] = f'attachment; filename={job_id}.pdf'
+        response['Content-Disposition'] = f'attachment; filename=demo.pdf'
         return response
-
-    # return JsonResponse({'job_id': job_id, 'path': path})
 
     async def get(self, request):
         return render(request, 'pdf/index_async.html')
