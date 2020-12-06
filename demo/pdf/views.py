b4f6933c992fd42da21a9d8c8cb447cc68a48a47
TK <dk@terminalkitten.com>
2020-12-06 14:34:51 +0100

    move view parts to pdf-reactor package

--- demo/pdf/views.py
+++ demo/pdf/views.py
@@ -1,39 +1,14 @@
-import pdfmate
-import asyncio
-
-from io import BytesIO
-
-from django.utils.decorators import classonlymethod
-
-from django.http import JsonResponse, HttpResponse
-from django.shortcuts import render
-from django.views.generic import View
+from pdf_reactor.views import PdfReactorAsyncView
 from pdf_reactor import PdfReactor
+
+from django.views.generic import View
+from django.http import JsonResponse
+from django.shortcuts import render
 
 reactor = PdfReactor()
 
 
-class PdfAsyncView(View):
-    @classonlymethod
-    def as_view(cls, **initkwargs):
-        view = super().as_view(**initkwargs)
-        view._is_coroutine = asyncio.coroutines._is_coroutine
-        return view
-
-    async def post(self, request):
-
-        path = await pdfmate.from_url(
-            request.build_absolute_uri(), None, options={'printBackground': True}
-        )
-
-        # get filestream async generated file
-        file_stream = BytesIO(path)
-
-        # generate downloadable file
-        response = HttpResponse(file_stream.getvalue(), content_type='application/pdf')
-        response['Content-Disposition'] = f'attachment; filename=demo.pdf'
-        return response
-
+class PdfAsyncView(PdfReactorAsyncView):
     async def get(self, request):
         return render(request, 'pdf/index_async.html')
 
